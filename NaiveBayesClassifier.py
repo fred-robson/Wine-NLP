2006e5dbdbe0baf567b1bfbd965b37aaa7169833
Fred Robson <frederickwlrobson@gmail.com>
2018-03-13 00:56:24 -0700

    updated naive bayes classifier

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -5,134 +5,70 @@ Implements Naive Bayes to run as a baseline for the wine project
 from data_utils import data_helper
 import numpy as np
 from collections import Counter,defaultdict
-from sklearn.metrics import confusion_matrix,f1_score
+import sklearn.metrics
+from sklearn import naive_bayes 
 import sys
 
 ALPHA_RANGE = [1,2,3,4,5,10]
 
-def classify(words,doc_counts,word_counts,len_vocab,alpha):
+
+def test_accuracy(Y_pred,Y_true):
+	acc = sklearn.metrics.accuracy_score(Y_pred,Y_true)
+	f1  = sklearn.metrics.f1_score(Y_pred,Y_true,average="macro")	
+	return acc,f1
+
+def run_NB(alpha,X,Y,NB,test_index = 1):
+	#test_index = 1 assumes that testing on dev set not test set 
+	naive_bayes = NB(alpha)
+	naive_bayes.fit(X[0],Y[0])
+	Y_pred = naive_bayes.predict(X[1])
+	Y_true = Y[1]
+	return test_accuracy(Y_pred,Y_true)
+
+def run_for_Y_cat(X,Y,Y_cat,NB,alpha_range):
 	'''
-	Classifies a single example - @words - according to the naive bayes model defined by 
-	doc_counts and word_counts
-
-	Return: The best label for the set of words
+	Assumes that X and Y are a tuple of three 
 	'''
+	print ("Category:",Y_cat)
+	print ("------------------------")
+	#print ("# Categories: ",len(doc_counts))
+	print ("Calculating Best Alpha using dev set...")
+	alpha_scores = {}
+	for alpha in alpha_range:
+		acc,f1 = run_NB(alpha,X,Y,NB)
+		alpha_scores[alpha] = f1
+		print("Alpha ",alpha,": Acc = ",acc,", F1 = ",f1)
+	best_alpha = max(alpha_scores,key=alpha_scores.get)
+	print("Best Alpha = ",best_alpha)
+	print ("Calculating Test accuracy")
+	acc,f1 = run_NB(best_alpha,X,Y,NB,2)
+	print ("\rTest Accuracy: ",acc)
+	print ("F1 Score: ",f1)
+	print ("\n\n")
 
-	doc_counts_sum = float(sum(doc_counts.values()))
-	all_LLs = {}
-
-	for label in doc_counts: 
-
-		#Get likelihood of document
-		log_likelihood = np.log(doc_counts[label]/doc_counts_sum)	
-		
-		word_counts_sum = sum(word_counts[label].values())
-		word_counts_sum += alpha*len_vocab
-		#Avoid integer division
-		word_counts_sum = float(word_counts_sum)
-
-		for w in words: 
-			w_count = word_counts[label][w]+alpha
-			log_likelihood += np.log(w_count/word_counts_sum)
-
-		all_LLs[label] = log_likelihood
-
-	return max(all_LLs,key=all_LLs.get)
-		
-
-def test_accuracy(X,Y,doc_counts,word_counts,len_vocab,alpha,print_iterations):
-	'''
-	Tests the accuracy of the Naive Bayes model on X,Y 
-	
-	Returns the accuracy, f1 score and confusion matrix
-	'''	
-	correct = 0.0 
-	incorrect = 0.0 
-
-	Y_pred = []
-	Y_true = []
-
-	for i,(x,true_label) in enumerate(zip(X,Y)): 
-		pred = classify(x,doc_counts,word_counts,len_vocab,alpha)
-		if true_label != None: 
-			Y_pred.append(pred)
-			Y_true.append(true_label)
-			if pred == true_label: correct+=1
-			else: incorrect+=1
-		if print_iterations:
-			if i % 1000 ==0: print("\rIteration ",i,": ",'%.5f'%(correct/(correct+incorrect)),end="\r")
-	return float(correct)/(incorrect+correct),f1_score(Y_true,Y_pred,average="macro"),confusion_matrix(Y_true,Y_pred)
-
-def get_best_alpha(X_dev,Y_dev,doc_counts,word_counts,len_vocab,alpha_range,show_iterations):
-	'''
-	Uses the dev set to get the best alpha 
-	'''
-	scores = {}
-	for a in alpha_range: 
-		accuracy,f1,_ = test_accuracy(X_dev,Y_dev,doc_counts,word_counts,len_vocab,a,show_iterations)
-		scores[a] = accuracy
-		print('\x1b[2K\r',end="\r") #Clears teh line
-		print("\rAlpha",a,'%.5f'%accuracy)
-
-	return max(scores,key=scores.get)
-
-def gen_dicts(X_train,Y_train):
-	'''
-	Converts the sentences into bag of words dicts {word: count}
-	'''
-	doc_counts = defaultdict(int)
-	word_counts = defaultdict(lambda: defaultdict(int))
-	for desc,label in zip(X_train,Y_train):
-		#skips the blanks
-		if type(label)not in [str,int,float]: continue
-		doc_counts[label]+=1
-		for w in desc: 
-			word_counts[label][w]+=1 
-	return doc_counts,word_counts
 
 
 def main(limit,show_iterations):
 	du = data_helper(limit)
-	len_vocab = len(du.generate_vocab_and_word_frequencies()[0])
 	
-	all_Y_cats = ["variety","points","price","country","province"]
-	
-	for Y_cat in all_Y_cats:
-		
-		if Y_cat == "price":
-			Y_train,Y_dev,Y_test =du.discretize(Y_cat)
-		else:	
-			Y_train,Y_dev,Y_test = du.get_Y_cat(Y_cat)
-	
-		doc_counts,word_counts = gen_dicts(du.X_train,Y_train)
-		print ("Category:",Y_cat)
-		print ("------------------------")
-		print ("# Categories: ",len(doc_counts))
-		print ("Calculating Best Alpha using dev set...")
-		alpha = get_best_alpha(du.X_train,Y_dev,doc_counts,word_counts,len_vocab,ALPHA_RANGE,show_iterations)
-		print("Best Alpha = ",alpha)
-		print ("Calculating Test accuracy")
-		accuracy,f1,confusion = test_accuracy(du.X_train,Y_test,doc_counts,word_counts,len_vocab,alpha,show_iterations)
-		print ("\rTest Accuracy: ",accuracy)
-		print ("F1 Score: ",f1)
-		print ("\n\n")
+	discrete_Y_cats = ["province","variety","country"]
+	cont_Y_cats = ["price","points"]
 
-		
+	for Y_cat in discrete_Y_cats: 
+		X_datasets,Y_datasets = du.filtered_on_missing_indices(Y_cat)
+		X_datasets = du.get_vectorized_X(X_datasets)
+		nb = naive_bayes.MultinomialNB
+		run_for_Y_cat(X_datasets,Y_datasets,Y_cat,nb,ALPHA_RANGE)
+	
 
 
 if __name__ == "__main__":
 	args = sys.argv
 	
 	limit = None
-	if str.isdigit(args[1]): 
+	if len(args)>0 and str.isdigit(args[1]): 
 		limit = int(args[1])
 		print("Limit of ",limit)
-
-	show_iterations = True
-	if "-q" in args:
-		show_iterations = False
-		print("Iterations turned off")
-	print ("\n")
 	main(limit,show_iterations)
 
 
--- data_utils.py
+++ data_utils.py
@@ -29,6 +29,7 @@ class data_helper():
 		self.X_dev = self.dev_data[X_cat]
 		self.X_test = self.test_data[X_cat]
 		self.vocab, self.word_freq_dict = self.generate_vocab_and_word_frequencies()
+		self.vocab_to_index = {v:i for i,v in enumerate(self.vocab)}
 
 	def load_data(self,max_len=None):
 		'''
@@ -49,7 +50,8 @@ class data_helper():
 			all_data = np.append(all_data,data[category])
 		return all_data
 
-	def get_Y_cat(self,Y_cat):
+	def get_Y_cat(self,Y_cat,normalize=False):
+		#TO DO: Add normalization
 		return self.train_data[Y_cat],self.dev_data[Y_cat],self.test_data[Y_cat]
 
 	def discretize(self,category,num_categories=20):
@@ -80,7 +82,54 @@ class data_helper():
 					discretized.append(str(min(set(x for x in bins if x>y))))
 			ret.append(discretized)
 
+		return ret
+
+	def missing_indices(self,Y_cat):
+		'''
+		Returns a vector of indices where the Y category is empty
+		'''	
+		ret = []
+		Y_data = self.get_Y_cat(Y_cat)
+		for Y in Y_data:
+			indices = []
+			for i,y_i in enumerate(Y):
+				if not type(y_i) is str and math.isnan(y_i): 
+					indices.append(i)
+			ret.append(indices)
 		return ret 
+
+	def filtered_on_missing_indices(self,Y_cat):
+		#Removes anything that has a missing Y value
+		X = self.X_train, self.X_dev, self.X_test
+		Y = self.get_Y_cat(Y_cat)
+		Z = self.missing_indices(Y_cat)
+	
+		ret = [],[]
+
+		for x,y,z in zip(X,Y,Z):
+			ret[0].append(np.delete(np.array(x),z))
+			ret[1].append(np.delete(np.array(y),z))
+		return ret
+	
+
+	def get_vectorized_X(self,all_X=None):
+		'''
+		Converts the ["I","am"...."am"] into a vector where the index represents a word count
+		'''
+		ret = []
+		if all_X is None: 
+			all_X =[self.X_train,self.X_dev,self.X_test] 
+			
+		for X  in all_X:
+			X_vectorized = np.zeros([len(X),len(self.vocab)])
+			for i,row in enumerate(X):
+				for w in row:
+					j = self.vocab_to_index[w]
+					X_vectorized[(i,j)]+=1
+			ret.append(X_vectorized)
+		return ret
+					
+	
 
 	def generate_vocab_and_word_frequencies(self):
 		'''
@@ -94,13 +143,13 @@ class data_helper():
 				if len(desc) > self.max_length:
 					self.max_length = len(desc)
 				for word in desc:
-					vocab|=set(word)
+					vocab.add(word)
 					word_counts[word]+=1 
 		return vocab, word_counts
 
 if __name__ == "__main__":
-	du = data_helper(300)
-	freq_dict = du.word_freq_dict
-	print (json.dumps(freq_dict, indent=1))
-	print(len(du.vocab))
+	du = data_helper(1000)
+	X,Y = du.filtered_on_missing_indices("province")
+	print (len(X[0]))
+	
 	#print(du.discretize("price",20))
