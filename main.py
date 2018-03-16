f7e3ea79912a07e3988680940d06578b779d58c3
Fred Robson <frederickwlrobson@gmail.com>
2018-03-15 20:03:24 -0700

    for different y categories

--- main.py
+++ main.py
@@ -7,47 +7,59 @@ import numpy as np
 import time
 from util import write_conll, print_sentence
 
-emb_helper = emb.embedding_helper(save_to_pickle = False, test_batch = 10000)
-data_helper = du.DataHelper(1000)
+def run_for_y_cat(y_cat,num_hidden=None,limit=None):
+	#initialize helpers
+	emb_helper = emb.embedding_helper(save_to_pickle = False, test_batch = 10000)
+	data_helper = du.DataHelper(limit)
+	label_helper_points = data_helper.labels_from_Y_cat(y_cat)
 
-X_train_df, X_dev_df = data_helper.X_train, data_helper.X_dev
-label_helper_points = data_helper.labels_from_Y_cat("points")
-vocab, _ = data_helper.generate_vocab_and_word_frequencies() 
-sub_emb_matrix, sub_tok2ind,sub_ind2tok, sub_unk_ind = emb_helper.get_sub_embeddings(vocab)
+	#Pull X data
+	X_train_df, X_dev_df = data_helper.X_train, data_helper.X_dev
+	vocab, _ = data_helper.generate_vocab_and_word_frequencies() 
+	X_train_tokens = X_train_df.as_matrix()
+	X_dev_tokens = X_dev_df.as_matrix()
 
-X_train_tokens = X_train_df.as_matrix()
-X_dev_tokens = X_dev_df.as_matrix()
+	#Gives appropriate indices for embeddings lookup 
+	sub_emb_matrix, sub_tok2ind,sub_ind2tok, sub_unk_ind = emb_helper.get_sub_embeddings(vocab)
+	X_train_indices = emb_helper.tok2ind_ind2tok(X_train_tokens, lookup_dict = sub_tok2ind, unk_indice = sub_unk_ind)
+	X_dev_indices = emb_helper.tok2ind_ind2tok(X_dev_tokens, lookup_dict = sub_tok2ind, unk_indice = sub_unk_ind)
+	embeddings = sub_emb_matrix
+	embeddings = np.asarray(embeddings)
 
-X_train_indices = emb_helper.tok2ind_ind2tok(X_train_tokens, lookup_dict = sub_tok2ind, unk_indice = sub_unk_ind)
-X_dev_indices = emb_helper.tok2ind_ind2tok(X_dev_tokens, lookup_dict = sub_tok2ind, unk_indice = sub_unk_ind)
-
-train_raw = [X_train_indices, label_helper_points.train_classes]
-dev_raw = [X_dev_indices, label_helper_points.dev_classes]
+	#Final data_set_up 
+	train_raw = [X_train_indices, label_helper_points.train_classes]
+	dev_raw = [X_dev_indices, label_helper_points.dev_classes]
 
 
+	#Configures the model 
+	config = Config("lstm", n_classes = label_helper_points.num_classes, many2one = True)
+	config.embed_size = embeddings.shape[1]
 
 
-config = Config("lstm", n_classes = label_helper_points.num_classes, many2one = True)
-embeddings = sub_emb_matrix
-embeddings = np.asarray(embeddings)
-config.embed_size = embeddings.shape[1]
+	#Runs the model 
+	with tf.Graph().as_default():
+	    print("Building model...",)
+	    start = time.time()
+	    model = RNNModel(data_helper, config, embeddings)
+	    print("took %.2f seconds", time.time() - start)
+	    
+	    init = tf.global_variables_initializer()
+	    saver = tf.train.Saver()
 
-with tf.Graph().as_default():
-    print("Building model...",)
-    start = time.time()
-    model = RNNModel(data_helper, config, embeddings)
-    print("took %.2f seconds", time.time() - start)
-    
-    init = tf.global_variables_initializer()
-    saver = tf.train.Saver()
+	    with tf.Session() as session:
+	        session.run(init)
+	        model.fit(session, saver, train_raw, dev_raw)
+	        output = model.output(session, dev_raw)
+	        sentences, class_labels, predictions = zip(*output)
+	        predictions = [[str(label_helper_points.class_2_lbl[cls]) for cls in preds] for preds in predictions]
+	        labels = [[str(label_helper_points.class_2_lbl[cls]) for cls in classes] for classes in class_labels]
+	        label_results = zip(labels, predictions)
+	        sentences = emb_helper.tok2ind_ind2tok(sentences, lookup_dict = sub_ind2tok, unk_indice = sub_unk_ind)
+	        output = zip(sentences, labels, predictions)
 
-    with tf.Session() as session:
-        session.run(init)
-        model.fit(session, saver, train_raw, dev_raw)
-        output = model.output(session, dev_raw)
-        sentences, class_labels, predictions = zip(*output)
-        predictions = [[str(label_helper_points.class_2_lbl[cls]) for cls in preds] for preds in predictions]
-        labels = [[str(label_helper_points.class_2_lbl[cls]) for cls in classes] for classes in class_labels]
-        label_results = zip(labels, predictions)
-        sentences = emb_helper.tok2ind_ind2tok(sentences, lookup_dict = sub_ind2tok, unk_indice = sub_unk_ind)
-        output = zip(sentences, labels, predictions)
+if __name__ == "__main__":
+	for y_cat in ["province","variety","country","points"]:
+		print(y_cat)
+		print("-----------------------------")
+		run_for_y_cat(y_cat,limit=500)
+		print("-----------------------------")
